91fcb10b000a4450173d0f23cf341ac1848a9a87
Evgeniy <estrin.j@yandex.ru>
2022-11-30 15:26:07 +0300

    dop

---
+++ z2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов с нечётными индексами.
+//[3, 7, 23, 12] -> 19 [-4, -6, 89, 6] -> 0
+
+void Zadacha36()
+{
+    Random rand = new Random();
+    int size = rand.Next(5,10);
+    int[] array = new int[size];
+    int count = 0;
+    Fill(array, -10, 10);
+    print(array);
+    for (int i = 0; i < size; i++)
+    {
+        if (i % 2 == 0&& i !=0) count = count + array[i];
+    }
+    Console.WriteLine();
+    
+   Console.WriteLine($"сумма чисел с четным индексом >  {count}");
+}
+
+void Fill(int[] massive, int min, int max)
+{
+    Random rand = new Random();
+    int size = massive.Length;
+    for (int i = 0; i < size; i++) massive[i] = rand.Next(min, max);
+}
+
+void print(int[] massive)
+{
+    for (int i = 0; i < massive.Length; i++) Console.Write($"  {massive[i]}");
+}
+Zadacha36();
---
+++ z3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу 
+//между максимальным и минимальным элементов массива.
+//3,21 7,04 22,93 -2,71 78,24] -> 80,95
+
+void Zadacha38()
+{
+    Random rand = new Random();
+    int size = rand.Next(5,10);
+    int[] array = new int[size];
+    int min = array[0]; int max =array[0];
+    Fill(array, -10, 10);
+    print(array);
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i]< min) min = array[i];
+        if (array[i]> max) max = array[i];
+    }
+    Console.WriteLine();
+    
+   Console.WriteLine($"разница между элементами  {max} - ({min}) = {max - min}");
+}
+
+void Fill(int[] massive, int min, int max)
+{
+    Random rand = new Random();
+    int size = massive.Length;
+    for (int i = 0; i < size; i++) massive[i] = rand.Next(min, max);
+}
+
+void print(int[] massive)
+{
+    for (int i = 0; i < massive.Length; i++) Console.Write($"  {massive[i]}");
+}
+Zadacha38();
