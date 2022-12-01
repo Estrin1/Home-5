e5c8f949ea75178af69754d594a370aa6e60ae55
Evgeniy <estrin.j@yandex.ru>
2022-12-01 15:41:41 +0300

    Dops

---
+++ dop2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 2. На вход подаются два числа случайной длины. 
+// Найдите произведение каждого разряда первого числа на каждый 
+// разряд второго. Ответ запишите в массив.
+// 24, 132 -> {2, 6, 4, 4, 12, 8}
+
+void DOP2()
+{
+    Console.WriteLine("Введите первое число");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе  число");
+    int y = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[FindCount(x) * FindCount(y)];
+    int c =0;
+    int size1 = FindCount(x);
+    int size = FindCount(y);
+    for (int i = 0; i < size1; i++)
+    {
+       for (int j = 0; j < size; j++)
+        {
+          array[c]= (x % 10) * (y % 10);
+          y= y/10;
+          c++;
+        }
+         x = x/10;
+        Console.WriteLine(x);
+    }
+    print(array);
+}
+
+
+
+//Описание методов
+
+int FindCount(int num1)
+{
+int count=0; 
+
+    while (num1 != 0)
+    {
+        num1 = num1 / 10;
+        count++;
+    }
+   return count; 
+}
+void print(int[] massive)
+{
+    for (int i = 0; i < massive.Length; i++) Console.Write($"  {massive[i]}");
+}
+
+DOP2();
---
+++ dop3/Program.cs
@@ -0,0 +1,78 @@
+﻿
+// Задача 3. Найдите все числа от 1 до 1000000, сумма цифр которых 
+// в три раза меньше их произведений. Подсчитайте их количество.
+
+void Zadacha3()
+{
+    int count = 0;
+    for (int i = 1; i <= 1000000; i++)
+    { 
+        if(FindMult(i)==FindSumm(i)+FindSumm(i)+FindSumm(i))
+        {
+            count++;
+            Console.Write($"  {i}");
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Количество чисел  " + count);
+}
+
+int FindSumm(int x)
+{
+    int summ = 0;
+    int number = 0;
+    while (x > 0)
+    {
+        number = x;
+        summ = summ + number % 10;
+        x = x / 10;
+    }
+    return summ;
+}
+
+
+int FindMult(int x)
+{
+    int Mult = 1;
+    while (x > 0)
+    {
+        
+        if (x % 10 != 0)
+        {
+            Mult *= x % 10;
+            x = x / 10;
+        }
+        else x = x / 10;
+    }
+    return Mult;
+}
+Zadacha3();
+
+// DOP3();
+
+//  int minNumber = 1;
+//         int maxNumber = 1000000;
+//         int gns_amount = 0;
+//         int amountNumbers = 0;
+
+//         Console.WriteLine("\nКоличество "Хороших" чисел в интервале от 1 до 1 000 000");
+//         for (int i = minNumber; i < maxNumber; i++)
+//         {
+//             amountNumbers = CalculateNumbers(i);
+//             if ((i % amountNumbers) == 0)
+//                 gns_amount++;
+//         }
+//         Console.WriteLine("составило {0:N0} штук", gns_amount);
+
+
+
+
+//     static int CalculateNumbers(int i)
+//     {
+//         int count = 0;
+//         while (i > 0)
+//         {
+//             i = i / 10;
+//             count++;
+//         }
+//         return count;
